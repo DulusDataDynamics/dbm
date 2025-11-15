281a0eef0fcf452cc8db114ec085d148295b3eca
Keshav Dulu <keshavdulu2@gmail.com>
2025-11-15 22:59:39 +0000

    delete the landing page    complely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
 "use client";
-import { useAuth } from "@/hooks/use-auth";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const { user, initializing } = useAuth();
-
-  if (initializing) {
-    return null; // The AuthProvider will show a global loader
-  }
-
-  if (user) {
-    redirect('/dashboard');
-  } else {
-    redirect('/login');
-  }
+  redirect('/dashboard');
 }
